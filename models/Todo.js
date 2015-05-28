bb8fe69f5f31301b1a35a7582aa8fdc252d5f529
Eric Templeton <erictempleton1@gmail.com>
2015-05-28 08:04:36 -0400

    trying new user method on schema

--- models/Todo.js
+++ models/Todo.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-var TodoSchema = mongoose.Schema({
-    created_by: String,
+var TodoSchema = new Schema({
+    created_by: { type: Schema.Types.ObjectId, ref: 'Account', required: true},
     name: String,
     completed: { type: Boolean, default: false },
     added: { type: Date, default: Date.now },
