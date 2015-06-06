6f7826369d6add1866cb39c47da0adbace5bce93
Eric Templeton <erictempleton1@gmail.com>
2015-06-06 14:23:07 -0400

    added created by check before deleting

--- routes/show_all.js
+++ routes/show_all.js
@@ -7,7 +7,7 @@ var Todo = require('../models/Todo.js');
 
 // GET todos listing
 router.get('/', function(req, res, next) {
-    Todo.find({created_by: req.user.username}, function (err, todos) {
+    Todo.find({created_by: req.user.username}, function(err, todos) {
         if (req.isAuthenticated())
             res.render('show_all.ejs', {
                 todos: todos
@@ -19,7 +19,11 @@ router.get('/', function(req, res, next) {
 
 // Delete todo
 router.delete('/:id', function(req, res, next) {
-    Todo.findByIdAndRemove(req.params.id, req.body, function(err, post) {
+    //Todo.findByIdAndRemove(req.params.id, req.body, function(err, post) {
+    Todo.remove({
+        _id: req.params.id,
+        created_by: req.user.username
+    }, function(err, todo_item) {
         if (err)
             return next(err);
         else
